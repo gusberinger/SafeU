985338ffc9f6ab8a77478708f6aeab32fa6faa52
unknown <gusberinger@gmail.com>
2020-12-05 13:13:14 -0600

    Added options for basemap and Type

--- Visualize.R
+++ Visualize.R
@@ -1,17 +1,29 @@
 library(tidyverse)
 library(leaflet)
-library(shiny)
 
-data <- read.csv("data/SafeU_geocoded.csv")
+data <- read.csv("data/SafeU_geocoded.csv", encoding = "latin-1")
 data <- data %>% mutate(Date=as.Date(Date, "%m-%d-%Y"),
                         Time=as.Date(Time, "%H:%M"))
 
-data %>% ggplot(aes(x = Date, y = Time)) + geom_point()
 
 
-icon.glyphicon <- makeAwesomeIcon(icon= 'flag', markerColor = 'blue', iconColor = 'black')
-icon.fa <- makeAwesomeIcon(icon = 'flag', markerColor = 'red', library='fa', iconColor = 'black')
-icon.ion <- makeAwesomeIcon(icon = 'home', markerColor = 'green', library='ion')
-data %>% filter(Type != "update") %>% leaflet() %>% addTiles() %>%
-  addMarkers(~long, ~lat, popup = ~Description, label = ~Type)
+displayTypes <- data %>% filter(Type != "Update") %>% group_by(Type) %>% summarize(n = n()) %>% arrange(n) %>% filter(n > 1) %>% pull(Type)
+data %>% 
+  filter(Type != "Update") %>%
+  mutate(Type = replace(Type, Type == "Shooting", "Other"),
+         Type = replace(Type, Type == "Chemical Spill", "Other"),
+         Type = replace(Type, Type == "Bomb Threat", "Other"),
+         Type = replace(Type, Type == "Rabies", "Other"),
+         Type = replace(Type, Type == "Barricade", "Other"),
+         Type = replace(Type, Type == "Chemical Spill", "Other")) %>%
+  leaflet() %>% 
+  addTiles() %>%
+  addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+  addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+  addMarkers(~long, ~lat, popup = ~Description, label = ~Type, group = ~Type) %>%
+  addLayersControl(
+    baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+    overlayGroups = c(displayTypes, "Other")
+  )
 
+data %>% ggplot(aes(x = Type)) + geom_histogram(stat="count")
